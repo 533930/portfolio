08c2a33e65910033387e7dcd0fb665c7d8097ae1
Luuk Wissink <533930@student.glu.nl>
2021-02-05 16:49:45 +0100

    final commit

---
+++ portfolio/resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="{{ url('../assets/style.css') }}">
+  </head>
+  <body>
+    <header>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/work">Work</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </header>
+    <div id="box1">
+      <div id="box2">
+        work preview
+      </div>
+      <div id="box5">
+        <img id="imghome" src="{{ url('../assets/img/ikzelf.jpg') }}" alt="">
+      </div>
+    </div>
+    <footer>
+        <h5>©Luuk Wissink</h5>
+        <a href="https://www.instagram.com/luukwis._/" target="_blank"><img id="iconf" src="{{ url('../assets/img/instagram.jpg') }}"></a>
+        <a href="https://twitter.com/luuk_wis" target="_blank"><img id="iconf" src="{{ url('../assets/img/twitter.jpg') }}"></a>
+        <a href="https://open.spotify.com/user/21wlywpvgortxakt6j6snlk7q?si=RlchLcExR-OHKg2WaUeByA" target="_blank"><img id="iconf" src="{{ url('../assets/img/spotify.jpg') }}"></a>
+    </footer>
+  </body>
+</html>
---
+++ portfolio/resources/views/work.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="{{ url('../assets/style.css') }}">
+  </head>
+  <body>
+    <header>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/work">Work</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </header>
+    <div id="box1">
+      <div id="box4">
+        <h1>webshop prodject</h1>
+      </div>
+      <div id="box6">
+        <img id="work" src="{{ url('../assets/img/work1.png') }}">
+      </div>
+      <div id="box3">
+        <img class="imgshop" src="{{ url('../assets/img/shop1.png') }}">
+        <img class="imgshop" src="{{ url('../assets/img/shop2.png') }}">
+        <img class="imgshop" src="{{ url('../assets/img/shop3.png') }}">
+        <img class="imgshop" src="{{ url('../assets/img/shop4.png') }}">
+      </div>
+    </div>
+    <footer>
+        <h5>©Luuk Wissink</h5>
+        <a href="https://www.instagram.com/luukwis._/" target="_blank"><img id="iconf" src="{{ url('../assets/img/instagram.jpg') }}"></a>
+        <a href="https://twitter.com/luuk_wis" target="_blank"><img id="iconf" src="{{ url('../assets/img/twitter.jpg') }}"></a>
+        <a href="https://open.spotify.com/user/21wlywpvgortxakt6j6snlk7q?si=RlchLcExR-OHKg2WaUeByA" target="_blank"><img id="iconf" src="{{ url('../assets/img/spotify.jpg') }}"></a>
+    </footer>
+    <script src="{{ url('../assets/script.js') }}"></script>
+  </body>
+</html>
